ee1f9f303416d6298fbfdef90688c7ae8ff806b6
Elerij777 <146883437+Elerij777@users.noreply.github.com>
2024-06-18 10:56:34 -0600

    Ruleta Bonais

---
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author Erick
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RuletaRifa si= new RuletaRifa();
+        si.setVisible(true);
+    }
+    
+}
